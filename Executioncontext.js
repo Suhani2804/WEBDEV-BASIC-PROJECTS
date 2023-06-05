b55e4817b4d705e2f6be82c300290b0e9e10f7c8
Suhani2804 <suhanigupta28@gmail.com>
2023-06-05 09:11:07 +0530

    change

--- Executioncontext.js
+++ Executioncontext.js
@@ -1,18 +1,32 @@
+////hoisting: able to allocate memory to elemnts even before assigning value
+//(HOISTING IS VERY IMPORTANT)
+
+//below is an example of function declaration as function is just declared without being stored in a variable
 console.log(a);  //undefined
 var a=10;
 var b=20;
-
 console.log(square(a));
 //above line will get executed even before we declare the function this is known as hoisting in javascript
-//hoisting: able to allocate memory to elemnts even before assigning value
-//(HOISTING IS VERY IMPORTANT)
-
 console.log(a); //10
-
 function square(num)
 {
-    num*num;
+    return num*num;
 }
+console.log(square1);
+console.log(square2);
+var square1=square(a);
+var square2=square(b);
+console.log(square1);
+console.log(square2);
 
-var square1=(a);
-var square2=(b);
+//below example is for function expression when the value of function is stored in a variable
+
+// console.log(xyz())----->this line gives error as function xyz is not declared. variable is declared and memory is alloacted for it and
+//hence it shows undefined
+console.log(z);
+var z=function xyz()
+{
+    console.log("javascript");
+}
+console.log(z);    //Output----->[Function: xyz]
+console.log(z());
