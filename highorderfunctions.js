75f4e56b2971a2549f49122a7c9f63b2922debd2
Suhani2804 <suhanigupta28@gmail.com>
2023-06-17 20:53:53 +0530

    change

--- highorderfunctions.js
+++ highorderfunctions.js
@@ -17,6 +17,7 @@ const obj=a.forEach(function(num)   //forEach does not return anything so we don
     console.log(num);
 })
 //for each does not return anything so below statement gives undefined
+console.log("forEach: ");
 console.log(obj);
 
 
@@ -30,6 +31,7 @@ const squaredarray= a.map(function(num)
 {
     return num*num;
 });
+console.log("map: ");
 console.log(squaredarray);
 //here we store the map value in an array as it returns an array so we need to store it
 
@@ -43,6 +45,7 @@ const evennumbers= z.filter(function(val)
 {
     return val % 2==0;
 });
+console.log("filter: ");
 console.log(evennumbers);
 // another method to do then same:
 // function iseven(val)
@@ -61,6 +64,7 @@ const result=transactions.some(function(num)
 {
     return num<0;
 })
+console.log("some: ");
 console.log(result);
 
 
@@ -72,6 +76,7 @@ const result2=transactions2.every(function(num)
 {
     return num>0;
 })
+console.log("every: ");
 console.log(result2);
 
 
@@ -89,6 +94,7 @@ const result3=b.reduce(function(acc,curr)  //0+1=1,1+2=3,3+3=6,6+4=10,10+5=15
 {
     return acc+curr;
 })
+console.log("reduce: ");
 console.log(result3);
 //acc---->accumulator is where the result is stored(like sum of for loop)....can give it any name
 //curr---->curr is the current array....can give it any name
@@ -101,11 +107,13 @@ const result4=s.find(function(val)
 {
     return val<0;
 })
+console.log("find: ");
 console.log(result4);
 
 
 //fill
+//use to change the content of given array from specified index
 let q=[1,2,3,-4,6];
-
 q.fill(5,1);
+console.log("fill: ");
 console.log(q);
